2d25c99d2325823021404277ac779cd440fb31f0
madde <madde@ec356a63957a>
2018-02-13 08:39:36 +0000

    initial commit

---
+++ src/test/java/se/uu/ub/cora/diva/mixedstorage/RecordStorageSpyData.java
@@ -0,0 +1,18 @@
+package se.uu.ub.cora.diva.mixedstorage;
+
+import se.uu.ub.cora.bookkeeper.data.DataGroup;
+
+public class RecordStorageSpyData {
+	public String type;
+	public String id;
+	public String calledMethod;
+	public DataGroup filter;
+	public Object answer;
+	public DataGroup record;
+	public DataGroup collectedTerms;
+	public DataGroup linkList;
+	public String dataDivider;
+
+	public RecordStorageSpyData() {
+	}
+}
